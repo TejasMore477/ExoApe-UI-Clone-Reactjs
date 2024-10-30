1667a76812678a28afc99e47f5c62ec2f4adcf0a
Tejas More <tejasmore660@gmail.com>
2024-10-30 18:43:29 +0530

    installed locomoitve, gsap & framermotion

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import Work from './components/work/Work'
 import PlayReel from './components/PlayReel/PlayReel'
 import ImagesSec from './components/spreadSection/ImagesSec'
 import Footer from './components/footer/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
+
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className='w-full overflow-x-hidden'>
       <Navbar/>
